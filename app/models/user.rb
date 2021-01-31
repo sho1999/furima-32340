054451b5e3f827924c03a5ba74eb1090296466d2
sho1999 <management14sho@gmail.com>
2021-01-31 19:11:36 +0900

    user function

---
+++ app/models/user.rb
@@ -0,0 +1,25 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+
+  validates :password, confirmation: true
+  validates :nickname        , presence: true
+  validates :email           , presence: true
+  validates :encrypted_password,     presence: true
+
+  with_options presence: true, format: { with: /\A[ぁ-ゔァ-ヴ\p{Ideographic}ａ-ｚＡ-Ｚ０-９]+\z/, message: 'に全角文字を使用してください' } do
+    validates :last_name
+    validates :first_name
+  end
+  
+  validates :last_name_kana  , presence: true
+  validates :first_name_kana , presence: true
+  validates :birthdate        , presence: true
+
+  has_many :items
+  has_many :item_users
+end
+
