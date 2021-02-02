e81fee970c0724a1a643fb08624df8a13e1d9f87
sho1999 <management14sho@gmail.com>
2021-02-02 21:33:34 +0900

    user_function_fix1

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,24 +6,26 @@ class User < ApplicationRecord
 
   VALID_PASSWORD_REGEX =/\A(?=.*?[a-z])(?=.*?[\d])\w{7,}\z/
 
-  validates :password             , confirmation: true,
-                                    presence: true,
-                                    format: { with: VALID_PASSWORD_REGEX}
-
-  validates :nickname             , presence: true
-  validates :email                , presence: true
-  validates :encrypted_password   , presence: true
-
-  with_options presence: true     , format: { with: /\A[ぁ-ゔァ-ヴ\p{Ideographic}ａ-ｚＡ-Ｚ０-９]+\z/, message: 'に全角文字を使用してください' } do
-    validates :last_name
-    validates :first_name
-  end
   
-  validates :last_name_kana       , presence: true
-  validates :first_name_kana      , presence: true
-  validates :birthdate            , presence: true
+  with_options presence: true do
+    validates :password confirmation: true, format: { with: VALID_PASSWORD_REGEX}
+
+    validates :nickname       
+    validates :email          
+    validates :encrypted_password
+
+    with_options format: { with: /\A[ぁ-ゔァ-ヴ\p{Ideographic}ａ-ｚＡ-Ｚ０-９]+\z/, message: 'に全角文字を使用してください' } do
+      validates :last_name
+      validates :first_name
+    end
+
+    validates :last_name_kana 
+    validates :first_name_kana
+    validates :birthdate
+  end
 
   has_many :items
   has_many :item_users
+  
 end
 
