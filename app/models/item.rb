ade605f96270f88c3b0ac76a2d9ab5b267d27cc6
sho1999 <management14sho@gmail.com>
2021-02-08 09:52:08 +0900

    fix

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,7 +10,7 @@ class Item < ApplicationRecord
       validates :postage_id
       validates :shipping_date_id
     end
-    validates :region_id, numericality: { other_than: 0, message: "Region Select" }
+    validates :region_id, numericality: { other_than: 0, message: "Select" }
     validates :price, :numericality => { :less_than => 10000000, :greater_than => 299, only_integer: true}, format: { with:/\A[0-9]+\z/}
     validates :image
   end
