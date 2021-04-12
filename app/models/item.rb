dd08561414fb610151e45d0b4f1077d90ff555d2
sho1999 <management14sho@gmail.com>
2021-04-12 22:14:23 +0900

    active hashの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,20 +1,31 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
 
   with_options presence: true do
     validates :name
     validates :description
     validates :price
     validates :image, unless: :was_attached?
-    validates :category_id
-    validates :state_id
-    validates :delivery_fee_id
-    validates :region_id
-    validates :shipping_date_id
+    with_options numericality: { other_than: 1 } do
+      validates :category_id
+      validates :state_id
+      validates :delivery_fee_id
+      validates :shipping_date_id
+    end
+    validates :region_id, numericality: { other_than: 0 } 
   end
+
 
   def was_attached?
     self.image.attached?
   end
+
+
+  belongs_to :category
+  belongs_to :state
+  belongs_to :delivery_fee
+  belongs_to :region
+  belongs_to :shipping_date
 
   belongs_to :user
   has_one_attached :image
