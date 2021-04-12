824c9720bd31a28675ace6923496766627bb3ac1
sho1999 <management14sho@gmail.com>
2021-04-12 20:45:06 +0900

    画像表示の実装と画像のみでもメッセージ送信可能となるバリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,11 +4,16 @@ class Item < ApplicationRecord
     validates :name
     validates :description
     validates :price
+    validates :image, unless: :was_attached?
     validates :category_id
     validates :state_id
     validates :delivery_fee_id
     validates :region_id
     validates :shipping_date_id
+  end
+
+  def was_attached?
+    self.image.attached?
   end
 
   belongs_to :user
