9e8546b7dc7f8a630d666fada69a82e9faae81b4
sho1999 <management14sho@gmail.com>
2021-04-18 20:17:31 +0900

    テストコード実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,15 +2,16 @@ FactoryBot.define do
   factory :item do
     name               {Faker::Name.initials(number: 3)}
     description        {Faker::Lorem.paragraph}
-    price              {Faker::Number.within(range: 300..999999999)}
+    price              {Faker::Number.within(range: 300..9999999)}
     category_id        {Faker::Number.within(range: 2..11)}
     state_id           {Faker::Number.within(range: 2..7)}
     delivery_fee_id    {Faker::Number.within(range: 2..3)}
     region_id          {Faker::Number.within(range: 1..47)}
-    shipping_date_id   {Faker::Number.within(range: 2..4)}
-
-    user               {Faker::Date.backward}
-    
+    shipping_date_id   {Faker::Number.within(range: 2..4)}    
     association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,84 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '出品機能' do
+    context 'When you can exhibit' do
+      it 'is valid with a name, description, price, category_id, state_id, delivery_fee_id, region_id, shipping_date_id, user' do
+        expect(@item).to be_valid
+      end
+    end
+
+    context 'When you cannot exhibit' do
+      it 'is invalid without a name' do
+        @item.name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
+      it 'is invalid without a description' do
+        @item.description = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Description can't be blank")
+      end
+      it 'is invalid without a price' do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+      it 'is invalid without a category_id' do
+        @item.category_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+      it 'is invalid without a state_id' do
+        @item.state_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("State can't be blank")
+      end
+      it 'is invalid without a delivery_fee_id' do
+        @item.delivery_fee_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery fee can't be blank")
+      end
+      it 'is invalid without a region_id' do
+        @item.region_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Region can't be blank")
+      end
+      it 'is invalid without a shipping_date_id' do
+        @item.shipping_date_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping date can't be blank")
+      end
+      it 'is invalid without a user_id' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("User must exist")
+      end
+      it 'is invalid without a image' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it '価格が299円以下だと出品できない' do
+        @item.price = '200'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be greater than 299")
+      end
+      it '価格が10000000円以上だと出品できない' do
+        @item.price = '10000000'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be less than 10000000")
+      end
+      it '販売価格は半角数値でないと出品できない' do
+        @item.price = '４００'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+    end
+  end
+
 end
