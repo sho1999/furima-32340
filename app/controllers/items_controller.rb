3ba82f9717f79772003e38ec889a3328e3b921c1
sho1999 <management14sho@gmail.com>
2021-04-22 22:15:50 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,8 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: [:show, :edit, :update]
-  before_action :move_to_index, only: [:new, :edit, :update]
   before_action :self_confirm, only: [:edit, :update]
+  before_action :authenticate_user!, except: [:show, :index]
+
   
   def index
     @item = Item.all.order("created_at DESC")
@@ -42,12 +43,6 @@ class ItemsController < ApplicationController
 
   def set_item
     @item = Item.find(params[:id])
-  end
-
-  def move_to_index
-    unless user_signed_in?
-      redirect_to items_path
-    end
   end
 
   def self_confirm
