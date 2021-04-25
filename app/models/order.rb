584e877874885914db476e65375d8d8d0b1b8f02
sho1999 <management14sho@gmail.com>
2021-04-25 15:05:41 +0900

    バリデーション作成

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,10 +1,12 @@
 class Order < ApplicationRecord
   with_options presence: true do
-    validates :postcode
+    VALID_POSTAL_CODE =/\A\d{3}[-]\d{4}\z/
+    VALID_PHONE_NUM =/\A\d{11}\z/
+    validates :postcode,   format: { with: VALID_POSTAL_CODE, message: "はハイフンあり7桁で入力して下さい"}
     validates :region_id
     validates :city
     validates :block
-    validates :phone_num
+    validates :phone_num,  format: { with: VALID_PHONE_NUM, message: "はハイフンなし11桁で入力して下さい"}
     validates :purchase 
   end
 
