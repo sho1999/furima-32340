5d092a9122de1db0020f0852c6f7bcfac18931d7
sho1999 <management14sho@gmail.com>
2021-04-25 18:27:38 +0900

    購入機能の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,6 +9,7 @@ class OrdersController < ApplicationController
   def create
     @order = Order.new(order_params)
     if @order.valid?
+      pay_item
       @order.save
       return redirect_to root_path
     else
@@ -19,11 +20,20 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.permit(:postcode, :region_id, :city, :block, :building, :phone_num)
+    params.require(:order).permit(:postcode, :region_id, :city, :block, :building, :phone_num).merge(token: params[:token])
   end
 
   def set_item
     @item = Item.find(params[:item_id])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]   # 自身のPAY.JPテスト秘密鍵を記述しましょう
+    Payjp::Charge.create(
+      amount: @item.price,  # 商品の値段
+      card: order_params[:token],    # カードトークン
+      currency: 'jpy'                 # 通貨の種類（日本円）
+    )
   end
 end
 
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,6 @@
 class Order < ApplicationRecord
+  attr_accessor :token
+
   with_options presence: true do
     VALID_POSTAL_CODE =/\A\d{3}[-]\d{4}\z/
     VALID_PHONE_NUM =/\A\d{11}\z/
@@ -7,9 +9,8 @@ class Order < ApplicationRecord
     validates :city
     validates :block
     validates :phone_num,  format: { with: VALID_PHONE_NUM, message: "はハイフンなし11桁で入力して下さい"}
-    validates :purchase 
   end
 
-  belongs_to :purchase
+  validates :token, presence: true
 end
 
