30409f9aaf7bd98d84476579aafbe37789e6ea7d
sho1999 <management14sho@gmail.com>
2021-04-25 18:32:10 +0900

    URL設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,9 @@
 class OrdersController < ApplicationController
 
   before_action :set_item, only: [:index, :create]
+  before_action :authenticate_user!
+  before_action :self_confirm
+
 
   def index
     @order = Order.new
@@ -34,6 +37,12 @@ class OrdersController < ApplicationController
       card: order_params[:token],    # カードトークン
       currency: 'jpy'                 # 通貨の種類（日本円）
     )
+  end
+
+  def self_confirm
+    if current_user.id != @item.user_id
+      redirect_to root_path
+    end
   end
 end
 
