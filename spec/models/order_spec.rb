3c8a44c03b285452d5b42df061822ac1ea5435b9
sho1999 <management14sho@gmail.com>
2021-04-26 20:46:59 +0900

    テストコード実装

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,58 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @order = FactoryBot.build(:order)
+  end
+
+  describe '購入機能' do
+    context 'When you can buy' do
+      it "全ての情報があれば保存できる" do
+        expect(@order).to be_valid
+      end
+    end
+
+    context 'When you cannot buy' do
+      it "postcodeが空では保存ができないこと" do
+        @order.postcode = ""
+        @order.valid?
+        expect(@order.errors.full_messages).to include("Postcode can't be blank")
+      end
+      it "region_idが空では保存ができないこと" do
+        @order.region_id = ""
+        @order.valid?
+        expect(@order.errors.full_messages).to include("Region can't be blank")
+      end
+      it "cityが空では保存ができないこと" do
+        @order.city = ""
+        @order.valid?
+        expect(@order.errors.full_messages).to include("City can't be blank")
+      end
+      it "blockが空では保存ができないこと" do
+        @order.block = ""
+        @order.valid?
+        expect(@order.errors.full_messages).to include("Block can't be blank")
+      end
+      it "phone_numが空では保存ができないこと" do
+        @order.phone_num = ""
+        @order.valid?
+        expect(@order.errors.full_messages).to include("Phone num can't be blank")
+      end
+      it "tokenが空では保存ができないこと" do
+        @order.token = ""
+        @order.valid?
+        expect(@order.errors.full_messages).to include("Token can't be blank")
+      end
+      it "postcodeは3桁-4桁でないと保存できないこと" do
+        @order.postcode = "1234567"
+        @order.valid?
+        expect(@order.errors.full_messages).to include("Postcode はハイフンあり7桁で入力して下さい")
+      end
+      it "phone_numは" do
+        @order.phone_num = "080-8978-1234"
+        @order.valid?
+        expect(@order.errors.full_messages).to include("Phone num はハイフンなし11桁で入力して下さい")
+      end
+    end
+  end
 end
