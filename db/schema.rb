e5339117e7f86fff321632f05f334fca6b043a4c
sho1999 <management14sho@gmail.com>
2021-05-01 18:41:15 +0900

    migrationファイルの名前を変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_26_121040) do
+ActiveRecord::Schema.define(version: 2021_04_27_111557) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -53,7 +53,7 @@ ActiveRecord::Schema.define(version: 2021_04_26_121040) do
     t.integer "region_id", null: false
     t.string "city", null: false
     t.string "block", null: false
-    t.string "building", null: false
+    t.string "building"
     t.string "phone_num", null: false
     t.bigint "purchase_id", null: false
     t.datetime "created_at", precision: 6, null: false
