58241e27f476ddfb4ab011b09285228b17fd2bf0
sho1999 <management14sho@gmail.com>
2021-05-01 12:05:44 +0900

    11桁のみから11桁以内に

--- app/models/user_purchase.rb
+++ app/models/user_purchase.rb
@@ -5,12 +5,12 @@ class UserPurchase
 
   with_options presence: true do
     VALID_POSTAL_CODE =/\A\d{3}[-]\d{4}\z/
-    VALID_PHONE_NUM =/\A\d{11}\z/
+    VALID_PHONE_REGEX = /\A\d{10}$|^\d{11}\z/
     validates :postcode,   format: { with: VALID_POSTAL_CODE, message: "はハイフンあり7桁で入力して下さい"}
     validates :region_id
     validates :city
     validates :block
-    validates :phone_num,  format: { with: VALID_PHONE_NUM, message: "はハイフンなし11桁で入力して下さい"}
+    validates :phone_num,  format: { with: VALID_PHONE_REGEX, message: "はハイフンなし11桁以内で入力して下さい"}
     validates :user_id
     validates :item_id
     validates :token
